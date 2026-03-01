600eba78fb22de4898545273f11565139f71f469
Balaganesh5307 <bg6951872@gmail.com>
2026-03-01 23:35:35 +0530

    Update CORS origin

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const dashboardRoute = require('./routes/dashboard');
 const app = express();
 
 app.use(cors({
-    origin: ["https://realcheck.onrender.com", "http://localhost:5173"],
+    origin: ["https://realcheck-1.onrender.com", "http://localhost:5173"],
     credentials: true
 }));
 app.use(express.json({ limit: '50mb' }));
